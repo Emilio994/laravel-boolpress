156f249ed3828c8be12feb3948f3a50cd8df4f3f
Emilio94 <emilio.994@gmail.com>
2021-05-19 15:28:15 +0200

    Compito

---
+++ Boolpress/database/seeds/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newPost = new Post();
+
+            $newPost->title = $faker->words(3, true);
+            $newPost->content = $faker->text(rand(50, 150));
+            $newPost->slug = Str::slug($newPost->title, '-');
+
+            $newPost->save();
+        }
+    }
+}
